ddd2ac9de7341ab9b8e8d36e549936cd8ce94185
ashishkanwal <ashukanwal121@gmail.com>
2024-08-11 19:50:07 +0530

    updated App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,44 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/shared/Navbar'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
+import Home from './components/Home'
+const approuter = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+   {
+    path:'/login',
+    element:<Login/>
+  },
+  {
+    path:'/signup',
+    element:<Signup/>
+  },
+  // {
+  //   path:'/',
+  //   element:<Home/>
+  // },
+  // {
+  //   path:'/',
+  //   element:<Home/>
+  // },
+  // {
+  //   path:'/',
+  //   element:<Home/>
+  // }
+])
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <>
-   <Navbar/>
-   </>
+   <div>
+  <RouterProvider router = {approuter}/>
+   </div>
   )
 }
 
