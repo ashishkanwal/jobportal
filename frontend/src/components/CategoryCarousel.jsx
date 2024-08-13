5ea69de262db5d45200a77429d163d95e46e9f08
Saurabh Singh Rautela <saurabhrautela786@gmail.com>
2024-08-13 21:22:09 +0530

    added category compo

---
+++ frontend/src/components/CategoryCarousel.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const category=[
+    "Frontend Developer",
+    "Backend Developer",
+    "Data Science",
+    "Grahpic Designer",
+    "FullStack Developer"
+]
+
+const CategoryCarousel = () => {
+  return (
+    <div>
+        //Hello
+    </div>
+  )
+}
+
+export default CategoryCarousel
