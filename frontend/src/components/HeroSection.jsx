9dd4a0cfb03c57d5d575f3b0483aaf4e620c8b2e
Saurabh Singh Rautela <saurabhrautela786@gmail.com>
2024-08-13 21:21:32 +0530

    added new compo

---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Button } from "./ui/button";
+import { Search } from "lucide-react";
+
+const HeroSection = () => {
+  return (
+    <div className="text-center ">
+      <div className="flex flex-col gap-5 my-10">
+        <span className="mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium">
+          Leading the Way in Job Search Excellence
+        </span>
+        <h1 className="text-5xl font-bold ">
+          Seek, Connect & Achieve <br /> Your Career{" "}
+          <span className="text-blue-500">Aspirations</span>
+        </h1>
+        <p className="text-gray-500">
+          Unlock your potential with opportunities that match your skills.
+        </p>
+        <div className="flex w-[40%] shadow-lg border-gray-200 pl-3 items-center gap-4 mx-auto  h-20 rounded-md">
+          <input
+            type="text"
+            placeholder="Find Your dream Jobs and your destination . . ."
+            className="outline-none border-none w-full h-10 px-2 text-xl "
+          />
+          <Button className="h-12 py-2 mx-1 mt-5 rounded-full bg-blue-500">
+            <Search className=" w-4" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
