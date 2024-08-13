f2a7010f31e9ddb8fe0e9e43ecfb0cb9770e1f5c
ashishkanwal <ashukanwal121@gmail.com>
2024-08-13 23:52:59 +0530

    added new components

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Navbar from "./shared/Navbar";
 import HeroSection from "./HeroSection";
 import CategoryCarousel from "./CategoryCarousel";
+import LatestJobs from "./LatestJobs";
+import TopCompanies from "./TopCompanies";
 
 function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Navbar />
       <HeroSection />
       <CategoryCarousel/>
+      <TopCompanies/>
+      <LatestJobs/>
     </div>
   );
 }
