67a8627bdd7ba0c557c771bde2d233d43c80a24a
Saurabh Singh Rautela <saurabhrautela786@gmail.com>
2024-08-14 21:25:00 +0530

    updates

--- frontend/src/components/LatestJobCards.jsx
+++ frontend/src/components/LatestJobCards.jsx
@@ -5,12 +5,12 @@ const LatestJobCards = () => {
   return (
     <div className='p-5  rounded-md shadow-xl bg-white border border-gray-100 cursor-pointer'>
         <div className=''>
-        <div className='mb-4 w-full flex justify-center'><img className='h-full w-20' src="https://th.bing.com/th/id/R.c117a0bda103aeb25c145a71b0b8ac5a?rik=mE5vIzQDJQrW9w&riu=http%3a%2f%2fpngimg.com%2fuploads%2fmicrosoft%2fmicrosoft_PNG10.png&ehk=kDRkokzjrya%2bERV6Vc5%2bbBAA7bwSDydXr7Cz5Spo4qk%3d&risl=&pid=ImgRaw&r=0" alt="" /></div>
-        <h1 className='font-medium text-lg '>Company Name</h1>
+        <div className='mb-4 w-full flex justify-center'><img className='h-full w-20' src="https://imgs.search.brave.com/8IpOqcqNRXc-08en11EmP8ryL9KNyg2xsx-Ju1tUkb0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvcHJl/dmlld3MvMDE5Lzc2/Ni8yNDAvbm9uXzJ4/L2FtYXpvbi1sb2dv/LWFtYXpvbi1pY29u/LXRyYW5zcGFyZW50/LWZyZWUtcG5nLnBu/Zw" alt="not found" /></div>
+        <h1 className='font-medium text-lg '>Amazon India</h1>
         <p className='text-sm text-gray-500'>India</p>
         </div>
         <div>
-            <h1 className='font-bold text-lg my-2'>Job Title</h1>
+            <h1 className='font-bold text-lg my-2'>Frontend Developer</h1>
             <p className='text-sm text-gray-600'> Lorem ipsum dolor sit amet consectetur </p>
         </div>
         <div className="flex items-center gap-2 mt-4">
