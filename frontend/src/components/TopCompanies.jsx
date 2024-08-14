9954ce03d0319fd09530ef224a0e3a7d8c0e9579
Saurabh Singh Rautela <saurabhrautela786@gmail.com>
2024-08-14 21:25:50 +0530

    complete

--- frontend/src/components/TopCompanies.jsx
+++ frontend/src/components/TopCompanies.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from './ui/carousel'
-import { Button } from './ui/button'
+import { IoIosArrowForward } from "react-icons/io";
 
 const company=["Google","Microsoft","Amazon","Flipkart"]
 const TopCompanies = () => {
@@ -11,11 +11,33 @@ const TopCompanies = () => {
         </div>
         <div>
       <Carousel className="w-5/6 max-w-6xl mx-auto my-20"> {/* Adjust the width as needed */}
-        <CarouselContent className="flex gap-2  h-52 ml-2"> {/* Adjust gap if needed */}
+        <CarouselContent className="flex gap-2  h-40 ml-1"> {/* Adjust gap if needed */}
           {
             company.map((cat, index) => (
-              <CarouselItem key={index} className="w-60 border-2 rounded-md"> {/* Ensure each item takes up 1/3 of the container */}
-                
+              <CarouselItem key={index} className="w-56 border-2 rounded-md px-0"> {/* Ensure each item takes up 1/3 of the container */}
+                <div className='w-full h-full py-2 flex flex-col gap-4 px-2'>
+                 <div>
+                 <div className='job-types flex gap-2 items-center'>
+                    <h1 className='font-semibold '>MNCs</h1>
+                    <IoIosArrowForward className='cursor-pointer text-black'/>
+                  </div>
+                  <small>1.9k+ are actively hiring</small>
+                 </div>
+                  <div className=' flex gap-1 h-full py-3'>
+                    <div className='w-12 h-12 bg-white rounded-md'>
+                      <img className='rounded-md h-full w-full' src="https://imgs.search.brave.com/5o9Ft_la8G3VBWruqyJMa1vaaBPBxzAeK8qsWJm4Y3w/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9sb2dv/ZGl4LmNvbS9pbWcv/bi5wbmc" alt="" />
+                    </div>
+                    <div className='w-12 h-12 bg-white rounded-md'>
+                    <img className='rounded-md h-full w-full' src="https://imgs.search.brave.com/KocevAFpfP4WwiHj5SrJdvQ17Edkuzes_fbaO7MpAFU/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9sb2dv/ZGl4LmNvbS9pbWcv/bS5wbmc" alt="" />
+                    </div>
+                    <div className='w-12 h-12 bg-white rounded-md'>
+                    <img className='rounded-md h-full w-full' src="https://imgs.search.brave.com/cjLpzo7rNkxz_DhdJzYDF0O3z0Qt1DBuVsQXh2I0_vI/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9sb2dv/ZGl4LmNvbS9pbWcv/Yi5wbmc" alt="" />
+                    </div>
+                    <div className='w-12 h-12 bg-white rounded-md'>
+                    <img className='rounded-md h-full w-full' src="https://imgs.search.brave.com/Ar0B7Vq4HTGvb1P-wdee0OsUVdN7qbp6DkBcy0FBaHg/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9sb2dv/ZGl4LmNvbS9pbWcv/aS5wbmc" alt="" />
+                    </div>
+                  </div>
+                </div>
               </CarouselItem>
             ))
           }
