b74f681862906ef6d10ff3fef88530d266d25828
ashishkanwal <ashukanwal121@gmail.com>
2024-08-15 23:11:33 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
 import Home from './components/Home'
+import Jobs from './components/Jobs'
 const approuter = createBrowserRouter([
   {
     path:'/',
@@ -18,6 +19,10 @@ const approuter = createBrowserRouter([
     path:'/signup',
     element:<Signup/>
   },
+   {
+    path:'/jobs',
+    element:<Jobs/>
+   },
   // {
   //   path:'/',
   //   element:<Home/>
@@ -25,11 +30,7 @@ const approuter = createBrowserRouter([
   // {
   //   path:'/',
   //   element:<Home/>
-  // },
-  // {
-  //   path:'/',
-  //   element:<Home/>
-  // }
+  //}
 ])
 
 function App() {
